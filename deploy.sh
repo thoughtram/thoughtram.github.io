136a5d9a881c70949f77da803f472b8fa8549c4a
Pascal Precht <pascal.precht@googlemail.com>
2014-10-16 00:23:29 +0200

    adds deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying new thoughtram site...\033[0m"
+
+# delete old gh-pages branch
+git branch -D deploy
+
+git checkout -b deploy
+
+# Add changes to git.
+git add -f dist
+
+# Commit changes.
+msg="chore(*): adding dist `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git subtree split -P dist -b deploy-dist
+
+# Push source and build repos.
+git push -f upstream deploy-dist:master
+git branch -D deploy-dist
+git checkout master
+git branch -D deploy
