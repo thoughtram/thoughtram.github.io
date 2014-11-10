ef585065fe44886e0eb0b02ab794a9ae11058941
Pascal Precht <pascal.precht@googlemail.com>
2014-11-10 15:48:16 +0100

    relaunch with loads of changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,13 @@
-/*!
- *
- *  Web Starter Kit
- *  Copyright 2014 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
 (function () {
   'use strict';
 
   var querySelector = document.querySelector.bind(document);
 
-  var navdrawerContainer = querySelector('.navdrawer-container');
+  var navdrawerContainer = querySelector('.thtrm-l-navdrawer');
   var body = document.body;
-  var appbarElement = querySelector('.app-bar');
-  var menuBtn = querySelector('.menu');
-  var main = querySelector('main');
+  var appbarElement = querySelector('.thtrm-l-header');
+  var menuBtn = querySelector('.thtrm-l-header__menu-btn');
+  var main = querySelector('.thtrm-l-main');
 
   function closeMenu() {
     body.classList.remove('open');
