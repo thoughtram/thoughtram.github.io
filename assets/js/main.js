10d5af7bb1fb50b7a89e3be72ea589fb54ac1c7c
Raphael Okon <raphael@okon.io>
2017-07-04 10:35:24 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const testimonialGalleryThumbnails = [...document.querySelectorAll(".js-thtrm-testimonial-trigger")]
+const testimonialGallery = new Flickity("#js-thtrm-testimonials", {
+	cellAlign: "left",
+	contain: true,
+	pageDots: false,
+	prevNextButtons: false
+})
+
+const toggleActiveModifierClass = htmlEl => htmlEl.classList.remove("is-active")
+const goToSlide = flktyInstance => slideNum => flktyInstance.select(slideNum, true, false)
+const goToSlideTestimonialGallery = goToSlide(testimonialGallery)
+
+testimonialGalleryThumbnails.forEach(el =>
+	el.addEventListener("click", () => {
+		const slideNum = +el.getAttribute("data-slide-num") - 1
+		goToSlideTestimonialGallery(slideNum)
+		testimonialGalleryThumbnails.forEach(toggleActiveModifierClass)
+		el.classList.add("is-active")
+	}));
