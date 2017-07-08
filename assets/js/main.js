181f607480607495cc85171b43053461071126a1
Raphael Okon <raphael@okon.io>
2017-07-08 07:29:09 +0200

    Adding thumbnail navigation sync to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const testimonialGalleryThumbnails = [...document.querySelectorAll(".js-thtrm-testimonial-trigger")]
 const testimonialGallery = new Flickity("#js-thtrm-testimonials", {
 	cellSelector: ".thtrm-testimonial",
@@ -7,14 +9,21 @@ const testimonialGallery = new Flickity("#js-thtrm-testimonials", {
 	prevNextButtons: false
 })
 
-const toggleActiveModifierClass = htmlEl => htmlEl.classList.remove("is-active")
+const addActiveModifierClass = htmlEl => htmlEl.classList.add("is-active")
+const removeActiveModifierClass = htmlEl => htmlEl.classList.remove("is-active")
 const goToSlide = flktyInstance => slideNum => flktyInstance.select(slideNum, true, false)
+const updateThumbnails = xs => activeIndex => {
+    xs.forEach(removeActiveModifierClass)
+    addActiveModifierClass(xs[activeIndex])
+}
 const goToSlideTestimonialGallery = goToSlide(testimonialGallery)
+const updateTestimonialGalleryThumbnails = updateThumbnails(testimonialGalleryThumbnails)
+
+testimonialGallery.on("select", () => updateTestimonialGalleryThumbnails(testimonialGallery.selectedIndex))
 
 testimonialGalleryThumbnails.forEach(el =>
 	el.addEventListener("click", () => {
 		const slideNum = +el.getAttribute("data-slide-num") - 1
 		goToSlideTestimonialGallery(slideNum)
-		testimonialGalleryThumbnails.forEach(toggleActiveModifierClass)
-		el.classList.add("is-active")
+        updateTestimonialGalleryThumbnails(slideNum)
 	}));
