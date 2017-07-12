c8ca0ce33f39676e4dbc5adaa3411a9e821e395a
Raphael Okon <raphael@okon.io>
2017-07-12 20:39:58 +0200

    Swapping slideshow implementation to Lory. Minor fixes for laptop layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,32 @@
 "use strict"
+document.addEventListener("DOMContentLoaded", function () {
+    const testimonialWrapper = document.querySelector("#js-thtrm-testimonials")
+    const testimonialGalleryThumbnails = [...document.querySelectorAll(".js-thtrm-testimonial-trigger")]
+    const testimonialGallery = lory(testimonialWrapper, {
+        enableMouseEvents: true,
+        slidesToScroll: 1,
+        slideSpeed: 500,
+        ease: "cubic-bezier(0.455, 0.03, 0.515, 0.955)"
+    })
+    const addActiveModifierClass = htmlEl => htmlEl.classList.add("is-active")
+    const removeActiveModifierClass = htmlEl => htmlEl.classList.remove("is-active")
+    const goToSlide = instance => slideNum => instance.slideTo(slideNum)
+    const updateThumbnails = xs => activeIndex => {
+        xs.forEach(removeActiveModifierClass)
+        addActiveModifierClass(xs[activeIndex])
+    }
+    const goToSlideTestimonialGallery = goToSlide(testimonialGallery)
+    const updateTestimonialGalleryThumbnails = updateThumbnails(testimonialGalleryThumbnails)
 
-const testimonialGalleryThumbnails = [...document.querySelectorAll(".js-thtrm-testimonial-trigger")]
-const testimonialGallery = new Flickity("#js-thtrm-testimonials", {
-	cellSelector: ".thtrm-testimonial",
-	cellAlign: "left",
-	contain: true,
-	pageDots: false,
-	prevNextButtons: false
-})
+    testimonialWrapper.addEventListener("after.lory.slide", ev => {
+        updateTestimonialGalleryThumbnails(ev.detail.currentSlide)
+    })
 
-const addActiveModifierClass = htmlEl => htmlEl.classList.add("is-active")
-const removeActiveModifierClass = htmlEl => htmlEl.classList.remove("is-active")
-const goToSlide = flktyInstance => slideNum => flktyInstance.select(slideNum, true, false)
-const updateThumbnails = xs => activeIndex => {
-    xs.forEach(removeActiveModifierClass)
-    addActiveModifierClass(xs[activeIndex])
-}
-const goToSlideTestimonialGallery = goToSlide(testimonialGallery)
-const updateTestimonialGalleryThumbnails = updateThumbnails(testimonialGalleryThumbnails)
+    testimonialGalleryThumbnails.forEach(el =>
+        el.addEventListener("click", () => {
+            const slideNum = +el.getAttribute("data-slide-num") - 1
+            goToSlideTestimonialGallery(slideNum)
+        }))
 
-testimonialGallery.on("select", () => updateTestimonialGalleryThumbnails(testimonialGallery.selectedIndex))
 
-testimonialGalleryThumbnails.forEach(el =>
-	el.addEventListener("click", () => {
-		const slideNum = +el.getAttribute("data-slide-num") - 1
-		goToSlideTestimonialGallery(slideNum)
-        updateTestimonialGalleryThumbnails(slideNum)
-	}));
+});
