eef65305dd74781f1c3687fac7cc3e49dbc75e0b
Pascal Precht <pascal.precht@gmail.com>
2018-07-23 14:57:42 +0200

    fix: make privacy statement GDPR compliant including ga opt-out script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -81,3 +81,17 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+const gaProperty = 'UA-51360648-2';
+
+const  disableStr = 'ga-disable-' + gaProperty;
+
+if (document.cookie.indexOf(disableStr + '=true') > -1) {
+  window[disableStr] = true;
+}
+
+function gaOptout() {
+  document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+  window[disableStr] = true;
+
+}
